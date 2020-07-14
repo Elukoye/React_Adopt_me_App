235c32b4a2a5bd2a348cf9b3d019c04d136fc60f
Elukoye <lukoyedith@gmail.com>
2020-07-15 00:29:13 +0300

    add Babel, .cache and .dist from parcel and Pet.js files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
-  ]);
-};
+import React from 'react';
+import {render} from 'react-dom'
+import { Pet } from './Pet';
+
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Bambi",
-      animal: "Dog",
-      breed: "Maltese",
-    }),
-    React.createElement(Pet, { name: "Tikka", animal: "Cat", breed: "Mixed" }),
-  ]);
+  // return React.createElement("div", {}, [
+  //   React.createElement("h1", {}, "Adopt Me!"),
+  //   React.createElement(Pet, {
+  //     name: "luna",
+  //     animal: "Dog",
+  //     breed: "Havanese",
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "Bambi",
+  //     animal: "Dog",
+  //     breed: "Maltese",
+  //   }),
+  //   React.createElement(Pet, { name: "Tikka", animal: "Cat", breed: "Mixed" }),
+  // ]);
+
+  return(
+    <div>
+      <h1>Adopt me</h1>
+      <Pet name = "Luna" animal ="Dog" breed = "Havanese" />
+      <Pet name = "Bambi" animal ="Dog" breed = "Maltese Terrier" />
+      <Pet name = "Tikka" animal ="Cat" breed = "Mixed" />
+    </div>
+  )
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(<App/>, document.getElementById("root"));
